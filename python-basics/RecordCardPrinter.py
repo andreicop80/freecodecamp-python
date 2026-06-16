db9738ebb66811490ac6b10747d8e187661681a1
Andrei <andreicop80@gmail.com>
2026-06-16 06:32:40 -0400

    Add core Python practice scripts to python-basics folder

---
+++ python-basics/RecordCardPrinter.py
@@ -0,0 +1,18 @@
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers, type(numbers))
+person = {'name': 'Alice', 'age': 20}
+print(person, type(person))         
+
